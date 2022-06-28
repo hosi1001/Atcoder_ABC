009e6b662238ca873fe793a0dd689edcc59a18f3
hosi <hosi1001@outlook.jp>
2022-06-28 22:00:09 +0900

    257 B

--- 257/B.cpp
+++ 257/B.cpp
@@ -39,34 +39,23 @@ int main(){
         }
 
         if (tmp + 1 < K ){
-
-
         if (A.at(tmp+1) == A.at(tmp) + 1){
             // printf("flag2\n");
             continue;
-        
-        } 
-        }
+        } }
 
         // printf("flag3\n");
-
         A.at(tmp) = A.at(tmp) +1 ;  
-        
-
     // for (int i = 0 ; i < K ; i++){
     // printf("%d " , A.at(i));
     // }
     // printf("\n");
-
-
-
     }
 
     for (int i = 0 ; i < K ; i++){
     printf("%d " , A.at(i));
     }
     return 0;
-
 
 
 
