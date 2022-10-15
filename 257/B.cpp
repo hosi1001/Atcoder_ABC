ea2b8627e313721a19e35f83cac861c52d84f567
hosi <hosi1001@outlook.jp>
2022-10-16 00:33:09 +0900

    257

---
+++ 257/B.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <iterator>
+#include <charconv>
+#include <bits/stdc++.h>
+#include<string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <set>
+using namespace std;
+
+int main() {
+    int N, K, Q;
+    cin >> N >> K >> Q;
+
+    vector<int> A(K);
+    vector<int> L(Q);
+    for (int i = 0; i < K; i++) {
+        cin >> A.at(i);
+    }
+    for (int i = 0; i < Q; i++) {
+        cin >> L.at(i);
+    }
+
+    for (int i = 0; i < Q; i++) {
+
+        int tmp;  // tmp >=0
+        tmp = L.at(i) - 1;
+
+        if (A.at(tmp) == N) {
+            // printf("flag1\n");
+            continue;
+        }
+
+        if (tmp + 1 < K) {
+            if (A.at(tmp + 1) == A.at(tmp) + 1) {
+                // printf("flag2\n");
+                continue;
+            }
+        }
+
+        // printf("flag3\n");
+    }
+    return 0;
+}
---
+++ 257/C.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <iterator>
+#include <charconv>
+#include <bits/stdc++.h>
+#include<string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <set>
+using namespace std;
+
+int main(){
+    int N , S;
+    cin >> N >> S; // S 大人0 子供1
+    int sum;
+    vector<int>W(N);
+    for (int i = 0 ; i < N ; i++){
+        sum = sum + W.at(i);
+    }
+
+    int ave = sum / N;
+
+    int cout = 0;
+    for (int i = 0 ; i < N ; i++){
+        
+    }
+
+    
+
+    return 0;
+
+
+
+
+}
