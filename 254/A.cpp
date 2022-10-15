420297f59ba95c196f351b7a9de1df118d39ac6f
hosi <hosi1001@outlook.jp>
2022-10-16 00:32:56 +0900

    254

---
+++ 254/A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <iterator>
+#include <charconv>
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+ 
+ 
+ 
+int main() {
+    int N;
+    int ans;
+    cin >> N ;
+    ans = N % 100;
+
+    printf("%02d", ans); 
+ 
+    return 0;
+    
+}
+ 
