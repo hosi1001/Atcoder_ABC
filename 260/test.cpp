1eb52688f51ccf8ef1376c4863a2d1d67e6f92fa
hosi <hosi1001@outlook.jp>
2022-10-16 00:34:25 +0900

    260

---
+++ 260/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <iterator>
+#include <charconv>
+#include <bits/stdc++.h>
+#include<string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <set>
+using namespace std;
+using ll = long long;
+
+
+int main(){
+    double matA [3][3];
+  for (int i = 0 ; i< 3 ; i++){
+      for (int j =0 ; j <3 ; j++){
+          scanf("%lf" ,matA[i][j]);
+      }
+  }
+
+  return 0;
+
+}
+//
+// Created by hosi1 on 2022/07/20.
+//
